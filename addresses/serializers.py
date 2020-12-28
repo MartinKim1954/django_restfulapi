bea0069371d1fcb289e1afc00d4757db92ed124a
MartinKim <kcs19542001@gmail.com>
2020-12-28 11:20:57 +0900

    dd?

---
+++ addresses/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Addresses
+
+
+class AddressesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Addresses
+        fields = ['name', 'phone_number', 'address', 'created']
